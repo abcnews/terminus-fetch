2c8471721e84b969dc45f38c759a2cbb7f27317e
Simon Elvery <simon@elvery.net>
2023-11-17 11:09:14 +1000

    Update tests and remove code related to Terminus v1

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,10 +2,7 @@ import { getImages } from './index';
 
 import * as v2_standard_complete from './test-data/v2-standard-complete';
 import * as v2_standard_proxy from './test-data/v2-standard-proxy';
-
-import * as v1_standard_complete from './test-data/v1-standard-complete';
-import * as v1_custom_complete from './test-data/v1-custom-complete';
-import * as v1_standard_proxy_title_update from './test-data/v1-standard-proxy-title-update';
+import * as v2_standard_proxy_with_default_ratio from './test-data/v2-standard-proxy-with-default-ratio';
 
 describe('getImages', () => {
   describe('Terminus v2', () => {
@@ -15,17 +12,8 @@ describe('getImages', () => {
     test('Standard image proxy with all relevant fields complete', () => {
       expect(getImages(v2_standard_proxy.input)).toEqual(v2_standard_proxy.result);
     });
-  });
-  describe('Terminus v1', () => {
-    test('Standard image with all the relevant fields complete', () => {
-      expect(getImages(v1_standard_complete.input)).toEqual(v1_standard_complete.result);
-    });
-    test('Standard image proxy with an updated title', () => {
-      expect(getImages(v1_standard_proxy_title_update.input)).toEqual(v1_standard_proxy_title_update.result);
-    });
-    test('Custom image with all the relevant fields complete', () => {
-      expect(getImages(v1_custom_complete.input)).toEqual(v1_custom_complete.result);
-      expect(getImages(v1_custom_complete.input, [100, 200])).toEqual(v1_custom_complete.result);
+    test('Standard image proxy with defaultRatio', () => {
+      expect(getImages(v2_standard_proxy_with_default_ratio.input).defaultRatio).toEqual('4x3');
     });
   });
 });
